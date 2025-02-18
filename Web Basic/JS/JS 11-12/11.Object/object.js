2e35a838758e89d462e8385948ea46282b478eba
LYS <sella45@naver.com>
2025-02-18 12:46:13 +0900

    it아카데미

--- Web Basic/JS/JS 11-12/11.Object/object.js
+++ Web Basic/JS/JS 11-12/11.Object/object.js
@@ -68,46 +68,64 @@ console.log('==============================');
     - 자바스크립트에서는 유효한 변수명일 경우 따옴표 생략 가능 
     - 유효한 변수명이 아니면 반드시 따옴표를 사용해야함 
 */
-
-
+var human = {
+    'first name' : '길동',
+    'last name' : '고',
+    age: 29
+}
+console.log(human);
 
 console.log('==============================');
 
 /*
     속성 값 읽기 : 
-    - 
-    - 
-    - 
-    - 
+    - .표기법과 []인덱싱 표기법으로 접근 가능 
+    - .표기법은 유효한 이름일 떄 사용 가능 
+    - []인덱싱 표기법은 모든 이름에서 표현 가능, 동적인 접근을 가능하게 해줌 
+    - 객체에 존재하지 않는 속성에 접근하려 하면 undefined를 반환 
 */
+console.log(human.age);
+console.log(human['age']);
+console.log(human['first name']);
 
-
+var property = 'last name';
+console.log(human[property]);
 
 console.log('==============================');
 
 /*
     속성 값 변경 :
-    - 
+    - 객체의 속성에 접근해서 새로운 값을 할당하면 변경 가능 
+    - 만약 존재하지 않는 속성에 접근하면 새로운 속성이 생성됨 
 */
 
-
-
 console.log('==============================');
-
 /*
     속성 삭제 :
-    - 
+    - delete 연산자를 사용하여 속성 제거 가능 
 */
+delete emptyObject.name;
+console.log(emptyObject);
+
+emptyObject.name = undefined;
+console.log(emptyObject);
 
 
 console.log('==============================');
-
 /*
     for-in 문 : 
-    - 
-    - 
+    - 객체에 포함된 키를 순차적으로 접근할 수 있음
+    - 문자열로된 속성의 키가 반환 
 */
+for(var key in saejo) {
+    console.log(saejo[key]);
+}
 
-
-
+var newSaejo = {};
+for (var key in saejo) {
+    newSaejo[key] = saejo[key];
+}
+newSaejo.name = '홍길동';
+console.log(newSaejo);
+console.log(saejo);
 console.log('==============================');
