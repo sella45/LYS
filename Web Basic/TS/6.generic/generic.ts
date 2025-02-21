544ef1eb3bcd3ee5af21e5f54469b90e9e5c01af
LYS <sella45@naver.com>
2025-02-21 16:18:16 +0900

    it아카데미

--- Web Basic/TS/6.generic/generic.ts
+++ Web Basic/TS/6.generic/generic.ts
@@ -1,9 +1,34 @@
 /*
     제너릭 : 
-    - 
-    - 
+    - 선언 시점이 아니라 컴파일 시점에 사용할 타입을 결정하는 방법론 
+    - 타입을 명시하지 않고 여러 타입을 사용할 수 있도록하여 코드의 재사용성과 안전성을 보장할 수 있음
 */
+interface Human1 {
+    name: string;
+    age: number;
+}
 
+interface Human2 {
+    name: string;
+    age: '어린이'|'청소년'|'청년'|'장년'|'노년';
+}
+
+interface Human3 {
+    name: string;
+    age: any;
+}
+
+interface Human<T> {
+    name: string;
+    age: T;
+}
+
+const human: Human<number> = {
+    name:'홍길동',
+    age: 20
+};
+
+// human.age.toFixed();
 
 
 console.log('==================================================');
