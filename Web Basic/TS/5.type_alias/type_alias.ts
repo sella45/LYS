1b58156ff0ed72bffacbb63643b70b9f002da3d1
LYS <sella45@naver.com>
2025-02-21 15:46:02 +0900

    it아카데미

--- Web Basic/TS/5.type_alias/type_alias.ts
+++ Web Basic/TS/5.type_alias/type_alias.ts
@@ -1,9 +1,76 @@
 /*
     타입 앨리어스 :
-    - 
-    - 
+    - 커스텀 타입을 지정하는 방법 (타입의 별칭 지정)
+    - type 키워드를 사용 
 */
+// let day : 'am' | 'pm';
+type Day = 'am' | 'pm' | null;
+let day1: Day = 'am';
 
-
+type int = number;
+let number: int = 10;
 
 console.log('==================================================');
+
+// 객체 타입 기정하는 방법 
+// 1. interface 이용 
+interface IKing {
+    name: string;
+    ho: string;
+    age: number;
+}
+
+const iking: IKing = {
+    name: '이성계',
+    ho:'태종',
+    age:74
+}
+
+// 2. tpye 사용 
+type TKing = {
+    name:string;
+    ho: string;
+    age: number;
+}
+
+const tking: TKing = {
+    name:'이방과',
+    ho: '정종',
+    age: 63
+}
+
+// 3. class 이용 
+class CKing {
+
+    constructor (
+    public name: string,
+    public ho: string,
+    public age: number
+    ) {}
+}
+
+const cking1: CKing = {
+    name: '이방원',
+    ho: '태종',
+    age: 56
+}
+
+const cking2: CKing = new CKing('이도','세종', 54);
+
+const king: IKing = tking;
+
+class C1 {
+    constructor(
+        public a: number,
+        public b: number
+    ){}
+}
+class C2 {
+    constructor(
+        public a: number,
+        public b: number
+    ){}
+}
+
+const ins1: C1 = { a: 10, b: 20};
+const ins2: C2 = ins1;
