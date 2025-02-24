e3b1111911f1eff3a943c16007dbd516f04d5571
LYS <sella45@naver.com>
2025-02-24 15:42:30 +0900

    it아카데미

--- react-basic/src/App.tsx
+++ react-basic/src/App.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 import { ClassComponent, FunctionalComponent } from './component/Component';
 import Component from './component/Component';
 import './App.css';
+import CurlyBraces from './component/CurlyBraces';
 
 
 function App() {
   return (
     <div>
-      <ClassComponent/>
+      {/* <ClassComponent/>
       <FunctionalComponent />
-      <Component />
+      <Component /> */}
+      <CurlyBraces/>
     </div>
   );
 }
