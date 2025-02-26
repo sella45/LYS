184d80f3c5b3ef838d8ddfa28ff9471b912b5e60
LYS(Mac) <sella45@naver.com>
2025-02-26 12:36:11 +0900

    it아카데미

--- react-basic/src/interaction/StateComponent.tsx
+++ react-basic/src/interaction/StateComponent.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { ChangeEvent, useState } from 'react'
 
 // 상태(state):
 // - 각 컴포넌트가 독립적으로 가지고 있는 데이터 기억 공간 
@@ -12,7 +12,12 @@ export default function StateComponent() {
     // - useState hook 함수로 상태를 선언 , hook함수를 쓰면 use를 앞에 쓴다 (컴포넌트 생명주기와 관련이 있음)
     // const [상태변수, 상태변경함수] = useState<상태변수타입> (초기값);
     const [count, setCount] = useState<number>(0);
-    let[num, setNumber] = useState<number>(0);
+    let [num, setNumber] = useState<number>(0);
+    const [total, setTotal] = useState<number>(0);
+
+    const[array, setArray] = useState<number[]>([]);
+
+    const [comment, setComment] = useState<string>('');
 
     const onClickHandler = () => {
         letCount++;
@@ -27,13 +32,86 @@ export default function StateComponent() {
         console.log('------------');
     }
 
+    const onStateClickHandler = () => {
+
+        // 상태변경함수를 사용하여 상태를 변경하더라도 바로 변경되지 않음
+        // 함수가 끝나고 렌더링되는 시점에 적용됨 
+        // console.log(count);
+        // setCount(count + 1);
+        // console.log(count);
+        // setCount(count + 1);
+        // console.log(count);
+        // setCount(count + 1);
+        // console.log(count);
+        // console.log('------------');
+
+        // let nextCount = count + 1;
+        // setCount(nextCount);
+        // nextCount += 1;
+        // setCount(nextCount);
+        // nextCount += 1;
+        // setCount(nextCount);
+
+        // 상태변경함수에 콜백함수를 전달하면 해당 콜백 함수를 누적시켜서 실행 
+        setCount((prevCount) => prevCount + 1);
+        setCount((prevCount) => prevCount + 1);
+        setCount((prevCount) => prevCount + 1);
+    };
+
+    const onSumHandler = () => {
+        // setCount(count + 1);
+        // setTotal(total + count);
+
+        const nextCount = count + 1;
+        setCount(nextCount);
+        const nextToal = total + nextCount;
+        setTotal(nextToal);
+    };
+
+    const onArrayAddHandler = () => {
+        // const nextCount = count + 1;
+        // setCount(nextCount);
+        const newArray = [...array, 1];
+        array.push(1);
+        setArray(newArray);
+    };
+
+    const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        const { value } = event.target;
+        setComment(value);
+    };
+
+    const onKeyDownHandler = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        const { key } = event;
+        if (key!== 'Enter') return;
+        onSubmitHandler();
+    };
+
+    const onSubmitHandler = () => {
+        alert('전송!');
+    };
 
     return (
         <div>
             <h2>일반변수 : {letCount}</h2>
             <h2>상태변수 : {count}</h2>
-            <h2>let 상태변수: {count} </h2>
+            <h2>let 상태변수: {num} </h2>
+            <h2> total :  {total}</h2>
             <button onClick={onClickHandler}>증가</button>
+            <button onClick={onStateClickHandler}>버튼</button>
+            <button onClick={onSumHandler}>더하기</button>
+            <div>
+                {array.join(', ')} / 길이: {array.length}
+                <button onClick={onArrayAddHandler}>추가</button>
+            </div>
+            <p>{comment}</p>
+            <input value={comment} onChange={onChangeHandler} />
+
+            <div style={{margin: '40px', padding: '40px', border: '1px solid gray'}}>
+                <input value={comment} onChange={onChangeHandler} onKeyDown={onKeyDownHandler}/>
+                <button onClick={onSubmitHandler}>전송</button>
+            </div>
+
         </div>
     )
 }
