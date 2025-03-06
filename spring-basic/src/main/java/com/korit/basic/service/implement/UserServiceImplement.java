d1162387cc87f1c2d1122197e59ff657e19d5df7
LYS(Mac) <sella45@naver.com>
2025-03-06 16:01:14 +0900

    it아카데미

--- spring-basic/src/main/java/com/korit/basic/service/implement/UserServiceImplement.java
+++ spring-basic/src/main/java/com/korit/basic/service/implement/UserServiceImplement.java
@@ -7,7 +7,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.korit.basic.dto.GetUserListResponseDto;
 import com.korit.basic.dto.GetUserResponseDto;
+import com.korit.basic.dto.PatchUserRequestDto;
 import com.korit.basic.dto.PostUserRequestDto;
 import com.korit.basic.dto.ResponseDto;
 import com.korit.basic.entity.UserEntity;
@@ -93,6 +95,22 @@ public class UserServiceImplement implements UserService {
   }
 
   @Override
+  public ResponseEntity<? super GetUserListResponseDto> getUserList() {
+    
+    List<UserEntity> userEntities = new ArrayList<>();
+
+    try {
+      userEntities = userRepository.findByOrderByUserIdAsc();
+    } catch(Exception exception) {
+      exception.printStackTrace();
+      return ResponseDto.databaseError();
+    }
+
+    return GetUserListResponseDto.success(userEntities);
+
+  }
+
+  @Override
   public ResponseEntity<? super GetUserResponseDto> getUser(String userId) {
 
     UserEntity userEntity = null;
@@ -106,6 +124,26 @@ public class UserServiceImplement implements UserService {
     }
     
     return GetUserResponseDto.success(userEntity);
+
+  }
+
+  @Override
+  public ResponseEntity<ResponseDto> patchUser(String userId, PatchUserRequestDto dto) {
+    
+    try {
+      UserEntity userEntity = userRepository.findByUserId(userId);
+      if (userEntity == null) return ResponseDto.noExistUser();
+
+      userEntity.setUserName(dto.getUserName());
+      userEntity.setUserAddress(dto.getUserAddress());
+      userRepository.save(userEntity);
+
+    } catch(Exception exception) {
+      exception.printStackTrace();
+      return ResponseDto.databaseError();
+    }
+
+    return ResponseDto.success(HttpStatus.OK);
 
   }
 
